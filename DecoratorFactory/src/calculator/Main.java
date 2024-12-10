787c64be85772a0a8636fd7a7a3658f0d2317bba
Ivan Mysovskii <vano641@mail.ru>
2024-12-10 19:30:28 +0300

    условие 2ой задачи

--- DecoratorFactory/src/calculator/Main.java
+++ DecoratorFactory/src/calculator/Main.java
@@ -1,8 +1,11 @@
 package calculator;
 
-/** Создать логирование операций, для класса Calculator. Класс Calculator не должен изменится.
+/**1) Создать логирование операций, для класса Calculator. Класс Calculator не должен изменится.
  * создали Loggable, LogCalculator, ConsoleLogger, LogCalculableFactory
- * 
+ * 2) Проект Observer: Добавить нового наблюдателя (сейчас два
+    наблюдателя Master и Student), организовать наблюдение за
+    объектом Company.
+
 */
 public class Main {
     public static void main(String[] args) {
